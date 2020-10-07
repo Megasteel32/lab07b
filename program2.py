98320390431ab0290cea3e90f8ecfa60d878928d
Megasteel32 <megasteel32@gmail.com>
2020-10-07 15:28:33 -0500

    ploop

--- program2.py
+++ program2.py
@@ -7,3 +7,12 @@
 # Assignment:   Lab 7 Program 2
 # Date:         10/06/2020
 
+user_word = input("Enter in a word: ")
+while user_word != "stop":
+    if user_word[0].lower() in ("a", "e", "i", "o", "u", "y"):
+        new_word = user_word + "yay"
+        print(new_word)
+    else:
+        new_word = user_word[1:] + user_word[0].lower() + "ay"
+        print(new_word)
+    user_word = input("Enter in a word: ")
