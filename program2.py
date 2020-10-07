152e3e922fd4831ea6ba9758b86d65e7017dc436
Megasteel32 <megasteel32@gmail.com>
2020-10-07 15:06:02 -0500

    ploop

---
+++ program2.py
@@ -0,0 +1,9 @@
+# By submitting this assignment, I agree to the following:
+#  Aggies do not lie, cheat, or steal, or tolerate those who do�
+#  I have not given or received any unauthorized aid on this assignment�
+#
+# Name:         Luca Maddaleni
+# Section:      273
+# Assignment:   Lab 7 Program 2
+# Date:         10/06/2020
+
